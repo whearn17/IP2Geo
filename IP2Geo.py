6c299103a60a0ef9783b0183b414bd5ab6c714f3
William Hearn <williamhearn17@gmail.com>
2023-06-29 13:54:42 -0400

    refactor
    add pyqt5 ui
    add dark mode
    remove csv output
    add config file
    add clipboard output

--- IP2Geo.py
+++ IP2Geo.py
@@ -1,15 +1,38 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QTextEdit, QPushButton, QMessageBox, QWidget
+from PyQt5.QtGui import QIcon
 import ipaddress
-import argparse
-import csv
 import requests
-import concurrent.futures
-import os
 import json
+import pyperclip
+import sys
 
 FIELDS = ['query', 'country', 'regionName', 'city', 'district', 'zip', 'isp', 'org', 'as',
           'mobile', 'proxy', 'status', 'message']
 
 CONFIG_FILE = 'config.json'
+
+DARK_STYLE = """
+    QWidget {
+        background-color: #303030;
+        color: #fff;
+    }
+
+    QPushButton {
+        background-color: #505050;
+        border: 1px solid #303030;
+    }
+
+    QPushButton:hover {
+        border: 1px solid #fff;
+    }
+
+    QTextEdit {
+        border: 2px solid #505050;
+        padding: 10px;
+        background-color: #303030;
+        color: #fff;
+    }
+"""
 
 
 def read_config():
@@ -35,11 +58,6 @@ def query_ip(ip, api_key):
         return None
 
 
-def write_to_csv(data, writer):
-    if data is not None:
-        writer.writerow(data)
-
-
 def is_valid_ip(ip):
     try:
         ipaddress.ip_address(ip)
@@ -48,41 +66,66 @@ def is_valid_ip(ip):
         return False
 
 
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.api_key = read_config()
+
+        self.setStyleSheet(DARK_STYLE)
+        self.setWindowTitle('IP2Geo')
+        self.setWindowIcon(QIcon('window_icon.png'))
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        lay = QVBoxLayout(self.central_widget)
+
+        self.ip_textbox = QTextEdit(self)
+        self.ip_textbox.setLineWrapMode(QTextEdit.NoWrap)
+        lay.addWidget(self.ip_textbox)
+
+        self.lookup_button = QPushButton('Lookup IPs', self)
+        self.lookup_button.clicked.connect(self.lookup_ips)
+        lay.addWidget(self.lookup_button)
+
+        self.setGeometry(100, 100, 500, 500)
+
+    def lookup_ips(self):
+        ips_text = self.ip_textbox.toPlainText().lstrip()  # Remove leading newline characters
+        ips = ips_text.split('\n')  # Split the input by line
+
+        results_cache = dict()  # Create a cache to store results of previous lookups
+
+        # Store results as list of strings to join later
+        results = [''] * len(ips)  # Initialize a list with the same length as the input, filled with empty strings
+
+        for i, ip in enumerate(ips):
+            if is_valid_ip(ip.strip()):  # We also strip each line to remove leading/trailing whitespaces
+                # Check the cache first
+                if ip in results_cache:
+                    results[i] = results_cache[ip]
+                else:
+                    data = query_ip(ip, self.api_key)
+                    if data and data.get('status') == 'success':
+                        result = "\t".join([data.get('country', ''), data.get('regionName', ''), data.get('isp', ''),
+                                            str(data.get('proxy', ''))])
+                        results[i] = result
+                        results_cache[ip] = result  # Add to cache
+
+        if not results:
+            QMessageBox.information(self, 'No results', 'No IP addresses were provided.')
+            return
+
+        result_string = '\n'.join(results)
+        pyperclip.copy(result_string)
+        QMessageBox.information(self, 'Copied to clipboard', 'The results have been copied to your clipboard.')
+
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", required=True, help="File containing IPs to query")
-    args = parser.parse_args()
-
-    api_key = read_config()
-    if not api_key:
-        return
-
-    try:
-        with open(args.input, 'r') as f:
-            ips = f.read().splitlines()
-    except FileNotFoundError:
-        print(f"Input file {args.input} not found.")
-        return
-
-    # Validate the IPs here and remove the invalid ones
-    ips = [ip for ip in ips if is_valid_ip(ip)]
-    if len(ips) == 0:
-        print("No valid IP addresses found.")
-        return
-
-    output_file = os.path.splitext(args.input)[0] + ".csv"
-    with concurrent.futures.ThreadPoolExecutor() as executor, open(output_file, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=FIELDS)
-        writer.writeheader()
-        future_to_ip = {executor.submit(query_ip, ip, api_key): ip for ip in ips}
-        for future in concurrent.futures.as_completed(future_to_ip):
-            ip = future_to_ip[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print(f"{ip} generated an exception: {exc}")
-            else:
-                write_to_csv(data, writer)
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    ex.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
